30385a0860792b20352c316bb5204f471af671ba
Joshua Nam <joshuasunwoonam@gmail.com>
2026-02-25 13:40:25 -0800

    Installed fireBase

--- Foodies/src/firebaseConfig.js
+++ Foodies/src/firebaseConfig.js
@@ -7,8 +7,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-
-  // enter your configuration code here
+  apiKey: "AIzaSyD-n6e1uDY65IgTyCIV1XK-Pjvs6Fvdbcg",
+  authDomain: "foodies-12b1c.firebaseapp.com",
+  projectId: "foodies-12b1c",
+  storageBucket: "foodies-12b1c.firebasestorage.app",
+  messagingSenderId: "812810355672",
+  appId: "1:812810355672:web:ca16ce5b324627ff60e8ad",
+  measurementId: "G-8NBLYF3J6X"
 };
 
 // Initialize Firebase
