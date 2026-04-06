adfbeda8e36320c15dc8d40543fcfb07f006201b
Joshua Nam <joshuasunwoonam@gmail.com>
2026-04-06 14:09:33 -0700

    Added previously cooked dishes. and improved home screen ui

--- Foodies/App.js
+++ Foodies/App.js
@@ -14,27 +14,55 @@ import DishIntroScreen from "./src/screens/DishIntroScreen";
 import IngredientScreen from "./src/screens/IngredientScreen";
 import CookingScreen from "./src/screens/CookingScreen";
 import CameraScreen from "./src/screens/CameraScreen";
+import CookedDishesScreen from "./src/screens/CookedDishesScreen";
+import { Ionicons } from "@expo/vector-icons";
 
 const ProtectedTab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 function ProtectedLayout({ photos }) {
   return (
-    <ProtectedTab.Navigator>
-      <ProtectedTab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{ headerShown: false }}
-      />
+    <ProtectedTab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "#FFF7F1",
+          borderTopWidth: 0,
+          height: 82,
+          paddingTop: 10,
+          paddingBottom: 12,
+        },
+        tabBarIcon: ({ focused, size }) => {
+          let iconName;
 
-      <ProtectedTab.Screen name="Profile" options={{ headerShown: false }}>
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Profile") {
+            iconName = focused ? "person" : "person-outline";
+          } else if (route.name === "Grocery") {
+            iconName = focused ? "cart" : "cart-outline";
+          }
+
+          return (
+            <Ionicons
+              name={iconName}
+              size={28}
+              color={focused ? "#F48F92" : "#9D9D9D"}
+            />
+          );
+        },
+      })}
+    >
+      <ProtectedTab.Screen name="Home" component={HomeScreen} />
+
+      <ProtectedTab.Screen name="Profile">
         {(props) => <ProfileScreen {...props} photos={photos} />}
       </ProtectedTab.Screen>
 
       <ProtectedTab.Screen
         name="Grocery"
         component={GroceryListScreen}
-        options={{ headerShown: false }}
       />
     </ProtectedTab.Navigator>
   );
@@ -71,6 +99,7 @@ export default function App() {
             <Stack.Screen name="DishIntro" component={DishIntroScreen} />
             <Stack.Screen name="Ingredients" component={IngredientScreen} />
             <Stack.Screen name="Cooking" component={CookingScreen} />
+            <Stack.Screen name="CookedDishes" component={CookedDishesScreen} />
             <Stack.Screen name="Camera">
               {(props) => <CameraScreen {...props} addPhoto={addPhoto} />}
             </Stack.Screen>
